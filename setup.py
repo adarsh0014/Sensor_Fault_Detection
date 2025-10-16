fa987741f18f909f0b2561706d9e0e44501b1786
Adarsh Keshrwani <adarshkeshrwani144@gmail.com>
2025-10-16 18:17:52 +0530

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HYPER_DOT_E  = '-e .'
+
+
 def get_requirements(file_path:str)->List[str]:
     requirement = []
     with open(file_path) as file_obj:
         requirement = file_obj.readlines()
         requirement = [req.replace('\n','') for req in requirement]
+
+    if HYPER_DOT_E in requirement:
+        requirement.remove(HYPER_DOT_E)
     
     return requirement
 
