65720454165a65814fb10ac411652d5e61662765
Adarsh Keshrwani <adarshkeshrwani144@gmail.com>
2025-10-22 13:08:19 +0530

    completed data ingestion

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src.exception import CustomException
 from src.logger import logging
 import sys
-from src.utils import dump_csvfile_to_mongodb_collection
-
+# from src.utils import dump_csvfile_to_mongodb_collection
+from src.pipeline.training_pipeline import TrainPipeline
 
 # def test_except():
 #     try:
@@ -13,10 +13,14 @@ from src.utils import dump_csvfile_to_mongodb_collection
 
 
 if __name__ =="__main__":
-    file_path = "D:\Programs\Data Science Projects\Sensor Fault Detection\\notebook\data\\aps_failure.csv"
-    database_name = 'ineuron'
-    collection_name = 'sensor'
-    dump_csvfile_to_mongodb_collection(filepath=file_path,database_name=database_name,collection_name=collection_name)
+    # file_path = "D:\Programs\Data Science Projects\Sensor Fault Detection\\notebook\data\\aps_failure.csv"
+    # database_name = 'ineuron'
+    # collection_name = 'sensor'
+    # dump_csvfile_to_mongodb_collection(filepath=file_path,database_name=database_name,collection_name=collection_name)
+
+
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
 
     # try:
     #     test_except()
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import os
-from src.constant  import training_pipeline
+from src.constant import training_pipeline
 
 class TrainingPipelineConfig:
    
@@ -39,3 +39,24 @@ class DataIngestionConfig:
             self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
 
             self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
