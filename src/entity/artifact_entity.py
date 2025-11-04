d574db0fccef53409fdb35c0092eb287cfc780c9
Adarsh Keshrwani <adarshkeshrwani144@gmail.com>
2025-11-04 18:21:09 +0530

    updated till model training

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -29,13 +29,13 @@ class DataTransformationArtifact:
 
 
 
-
-
 @dataclass
 class ClassificationMetricArtifact:
     f1_score: float
     precision_score: float
     recall_score: float
+
+
 
 @dataclass
 class ModelTrainerArtifact:
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,11 +1,12 @@
 from src.exception  import CustomException
-from src.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
-from src.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from src.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
+from src.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from src.logger import logging
 import sys , os 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 
 class TrainPipeline:
@@ -58,6 +59,21 @@ class TrainPipeline:
             return data_transformation_artifact
         except  Exception as e:
             raise  CustomException(e,sys)
+        
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact):
+        try:
+            model_trainer_config = ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
+
+            model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+            
+            return model_trainer_artifact
+        
+        
+        except  Exception as e:
+            raise  CustomException(e,sys)
 
 
 
@@ -66,5 +82,7 @@ class TrainPipeline:
              data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
              data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
              data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
+             
         except Exception as e :    
             raise  CustomException(e,sys)
